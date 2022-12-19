dc8fb39fc45837aab19ed23cb632052f32ace3b2
alfianifk <alfiani.fk@gmail.com>
2022-12-19 22:28:40 +0700

    add file and folder

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://mellifluous-hotteok-922ced.netlify.app/" // TODO: replace this
+const studentName = "ALFIANIFITRIAKUSNADI" // TODO: replace this
+const studentId = "FE2649464" // TODO: replace this
+
+export { url, studentName, studentId }
